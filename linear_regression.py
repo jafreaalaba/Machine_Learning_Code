30e4aa736e70f7d01db3bc250e30491b75b3fac9
jafreaalaba <jafreaalaba@gmail.com>
2024-10-02 15:41:21 -0600

    Linear_Regression

--- linear_regression.py
+++ linear_regression.py
@@ -5,18 +5,17 @@ from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
 np.random.seed(34)
-
+   
 #Generate synthetic data
-X = 2 * np.random.rand(100,1) # Generate 100 random numbers between 0 & 2
-y = 4 + 4 * X + np.random.randn(100,1) #Linear relationship with noise
+X = 2 * np.random.rand(100, 1)  # Generate 100 random numbers between 0 & 2
+y = 4 + 4 * X + np.random.randn(100, 1) #Linear relationship with noise
 
-X_train, X_test, y_train, y_test = train_test_split (X, y, test_size=0.2,random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=42)
 
-model= LinearRegression()
+model = LinearRegression()
 model.fit(X_train, y_train)
 
 y_prediction = model.predict(X_test)
-
 
 mse = mean_squared_error(y_test, y_prediction)
 
@@ -24,7 +23,17 @@ plt.scatter(X_test, y_test, color="blue")
 plt.plot(X_test, y_prediction, color="red")
 plt.xlabel('X')
 plt.ylabel('y')
+plt.title(mse)
 plt.show()
 
-print('Mean Squared Error:', mse)
+#print('Mean Squared Error:', mse)
+
+
+
+
+
+
+
+
+
 
